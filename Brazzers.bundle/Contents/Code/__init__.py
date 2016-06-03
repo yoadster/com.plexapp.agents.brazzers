3c41828ecbf8dc11f8d3afb13ad3905264638082
yoadster <leebrennan@me.com>
2016-06-03 08:06:09 +0100

    DOM Changes
    Fixed issue where matches were not found due to DOM element changes on
    the site.

--- Brazzers.bundle/Contents/Code/__init__.py
+++ Brazzers.bundle/Contents/Code/__init__.py
@@ -19,6 +19,27 @@ def any(s):
 def Start():
     HTTP.CacheTime = CACHE_1DAY
 
+def capitalize(line):
+    return ' '.join([s[0].upper() + s[1:] for s in line.split(' ')])
+
+def tagAleadyExists(tag,metadata):
+    for t in metadata.genres:
+        if t.lower() == tag.lower():
+            return True
+    return False
+
+def posterAlreadyExists(posterUrl,metadata):
+    for p in metadata.posters.keys():
+        Log(p.lower())
+        if p.lower() == posterUrl.lower():
+            Log("Found " + posterUrl + " in posters collection")
+            return True
+
+    for p in metadata.art.keys():
+        if p.lower() == posterUrl.lower():
+            return True
+    return False
+
 class EXCAgent(Agent.Movies):
     name = 'Brazzers'
     languages = [Locale.Language.English]
@@ -41,7 +62,7 @@ class EXCAgent(Agent.Movies):
         encodedTitle = urllib.quote(title)
 
         searchResults = HTML.ElementFromURL('http://www.brazzers.com/search/all/?q=' + encodedTitle)
-        for searchResult in searchResults.xpath('//div[contains(@class,"scene-card-title")]//a'):
+        for searchResult in searchResults.xpath('//h2[contains(@class,"scene-card-title")]//a'):
             Log(str(searchResult.get('href')))
             titleNoFormatting = searchResult.get('title')
             curID = searchResult.get('href').replace('/','_')
@@ -54,15 +75,18 @@ class EXCAgent(Agent.Movies):
     def update(self, metadata, media, lang):
 
         Log('******UPDATE CALLED*******')
+        zzseries = False
         metadata.studio = 'Brazzers'
         temp = str(metadata.id).replace('_','/')
         url = 'http://www.brazzers.com' + temp
         detailsPageElements = HTML.ElementFromURL(url)
 
         # Summary
-        paragraph = detailsPageElements.xpath('//p')[1].text_content()
-        metadata.summary = paragraph.replace('&13;', '').strip(' \t\n\r"') + "\n\n"
+        paragraph = detailsPageElements.xpath('//p[@itemprop="description"]')[0].text_content()
+        metadata.summary = paragraph.replace('&13;', '').strip(' \t\n\r"').replace('\n','').replace('  ','') + "\n\n"
         tagline = detailsPageElements.xpath('//span[@class="label-text"]')[0].text_content()
+        if tagline == 'ZZ Series':
+            zzseries = True
         metadata.tagline = str(tagline)
         metadata.title = detailsPageElements.xpath('//h1')[0].text_content()
 
@@ -73,50 +97,63 @@ class EXCAgent(Agent.Movies):
         if Prefs["excludegenre"] is not None:
             Log("exclude")
             genreFilter = Prefs["excludegenre"].split(';')
-            
-        genreFilter.append("Big Dick Worship")
-        genreFilter.append("Big Tits Worship")
-        genreFilter.append("DP (Standing)")
-        genreFilter.append("Missionary")
 
         genreMaps=[]
         genreMapsDict = {}
 
         if Prefs["tagmapping"] is not None:
-            Log("tagmapping")
             genreMaps = Prefs["tagmapping"].split(';')
             for mapping in genreMaps:
                 keyVal = mapping.split("=")
-                genreMapsDict[keyVal[0]] = keyVal[1]
+                genreMapsDict[keyVal[0]] = keyVal[1].lower()
         else:
             genreMapsDict = None
 
-        Log(genreMapsDict);
-    
         if len(genres) > 0:
             for genreLink in genres:
-                genreName = genreLink.text_content().strip('\n')
+                genreName = genreLink.text_content().strip('\n').lower()
                 if any(genreName in g for g in genreFilter) == False:
                     if genreMapsDict is not None:
                         if genreName in genreMapsDict:
-                            Log(genreMapsDict[genreName])
-                            metadata.genres.add(genreMapsDict[genreName])
+                            if not tagAleadyExists(genreMapsDict[genreName],metadata):
+                                metadata.genres.add(capitalize(genreMapsDict[genreName]))
                         else:
-                            Log('Not Mapped')
-                            metadata.genres.add(genreName)
+                            if not tagAleadyExists(genreName,metadata):
+                                metadata.genres.add(capitalize(genreName))
                     else:
-                        metadata.genres.add(genreName)
+                        metadata.genres.add(capitalize(genreName))
 
-        date = detailsPageElements.xpath('//aside[contains(@class,"scene-date")]')[0].text_content()
-        date_object = datetime.strptime(date, '%B %d, %Y')
-
-        metadata.originally_available_at = date_object
-        metadata.year = metadata.originally_available_at.year
-
+        if not zzseries:
+            date = detailsPageElements.xpath('//aside[contains(@class,"scene-date")]')[0].text_content()
+            date_object = datetime.strptime(date, '%B %d, %Y')
+            metadata.originally_available_at = date_object
+            metadata.year = metadata.originally_available_at.year
+        else:
+            for wrapper in detailsPageElements.xpath('//div[@class="release-card-wrap"]'):
+                Log('wrapper')
+                cardTitle = wrapper.xpath('.//div[@class="card-image"]//a')[0].get('title')
+                Log(cardTitle)
+                if cardTitle.lower() == metadata.title.lower():
+                    Log('match')
+                    date = detailsPageElements.xpath('..//time')[0].text_content()
+                    date_object = datetime.strptime(date, '%B %d, %Y')
+                    metadata.originally_available_at = date_object
+                    metadata.year = metadata.originally_available_at.year 
+                    metadata.roles.clear()
+                    metadata.collections.clear()
+                    starring = wrapper.xpath('.//div[@class="model-names"]//a')
+                    for member in starring:
+                        role = metadata.roles.new()
+                        role.actor = member.get('title').strip()
+                        metadata.collections.add(member.get('title').strip())
+                    p = wrapper.xpath('.//div[@class="card-image"]//img')[0].get('src')
+                    Log(p)
+                    metadata.posters[p] = Proxy.Preview(HTTP.Request(p, headers={'Referer': 'http://www.google.com'}).content, sort_order = 1)
+                        
         # Starring/Collection
         # Create a string array to hold actors
         maleActors=[]
-
+        
         # Refresh the cache every 50th query
         if('cache_count' not in Dict):
             Dict['cache_count'] = 0
@@ -130,7 +167,7 @@ class EXCAgent(Agent.Movies):
                 Dict['cache_count'] = str(cache_count + 1)
                 Dict.Save()
                 Log(str(cache_count))
-        
+          
         if('actors' not in Dict):
             Log('******NOT IN DICT******')
             maleActorHtml = None
@@ -155,36 +192,20 @@ class EXCAgent(Agent.Movies):
             addActors = Prefs['excludeactor'].split(';')
             for a in addActors:
                 maleActors.append(a)
-            
-        #maleActors.append('James Maverick')
-        #maleActors.append('Prince Yashua')
-        #maleActors.append('Karlo Karerra')
-        #maleActors.append('Brick Danger')
-        #maleActors.append('Mr. Pete')
-        #maleActors.append('Cage')
-        
-        metadata.roles.clear()
-        metadata.collections.clear()
-        
+      
         #starring = None
-        starring = detailsPageElements.xpath('//ul[contains(@class,"related-model")]//a')
+        if not zzseries:
+            metadata.roles.clear()
+            metadata.collections.clear()
+            starring = detailsPageElements.xpath('//p[contains(@class,"related-model")]//a')
 
-        for member in starring:
-            # Check if member exists in the maleActors list as either a string or substring
-            if any(member.text_content().strip() in m for m in maleActors) == False:
-                role = metadata.roles.new()
-                # Add to actor and collection
-                role.actor = member.text_content().strip()
-                metadata.collections.add(member.text_content().strip())
-                #try:
-                url = 'http://www.brazzers.com' + member.get('href')
-                Log(url)
-                actorDetails = HTML.ElementFromURL(url)
-                
-                actorPhoto = actorDetails.xpath('//img')
-                
-                role.photo = actorPhoto[5].get('src')
-                #except:pass
+            for member in starring:
+                # Check if member exists in the maleActors list as either a string or substring
+                if any(member.text_content().strip() in m for m in maleActors) == False:
+                    role = metadata.roles.new()
+                    # Add to actor and collection
+                    role.actor = member.text_content().strip()
+                    metadata.collections.add(member.text_content().strip())
 
         #Rating
         #try:
@@ -197,22 +218,28 @@ class EXCAgent(Agent.Movies):
         #except:pass
 
         #Posters
-        i = 1
-        for poster in detailsPageElements.xpath('//a[@rel="preview"]'):
-            posterUrl = poster.get('href').strip()
-            thumbUrl = detailsPageElements.xpath('//img[contains(@src,"thm")]')[i-1].get('src')
+        if not zzseries:
+            i = 1
+            for poster in detailsPageElements.xpath('//a[@rel="preview"]'):
+                posterUrl = poster.get('href').strip()
+                thumbUrl = detailsPageElements.xpath('//img[contains(@src,"thm")]')[i-1].get('src')
+                if not posterAlreadyExists(posterUrl,metadata):            
+                    #Download image file for analysis
+                    img_file = urllib.urlopen(posterUrl)
+                    im = StringIO(img_file.read())
+                    resized_image = Image.open(im)
+                    width, height = resized_image.size
 
-            #Download image file for analysis
-            img_file = urllib.urlopen(posterUrl)
-            im = StringIO(img_file.read())
-            resized_image = Image.open(im)
-            width, height = resized_image.size
-
-            #Add the image proxy items to the collection
-            if(width == 533):
-                # Item is a poster
-                metadata.posters[posterUrl] = Proxy.Preview(HTTP.Request(thumbUrl, headers={'Referer': 'http://www.google.com'}).content, sort_order = i)
-            if(width == 800):
-                # Item is an art item
-                metadata.art[posterUrl] = Proxy.Preview(HTTP.Request(thumbUrl, headers={'Referer': 'http://www.google.com'}).content, sort_order = i)
-            i += 1
+                    #Add the image proxy items to the collection
+                    if(width == 533):
+                        # Item is a poster
+                        metadata.posters[posterUrl] = Proxy.Preview(HTTP.Request(thumbUrl, headers={'Referer': 'http://www.google.com'}).content, sort_order = i)
+                    if(width == 800):
+                        # Item is an art item
+                        metadata.art[posterUrl] = Proxy.Preview(HTTP.Request(thumbUrl, headers={'Referer': 'http://www.google.com'}).content, sort_order = i)
+                    i += 1
+        else:
+            
+            background = detailsPageElements.xpath('//*[@id="trailer-player"]/img')[0].get('src')
+            Log(background)
+            metadata.art[background] = Proxy.Preview(HTTP.Request(background, headers={'Referer': 'http://www.google.com'}).content, sort_order = 1)
